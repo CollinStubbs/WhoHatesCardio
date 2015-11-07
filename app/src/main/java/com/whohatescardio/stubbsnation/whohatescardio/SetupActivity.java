c099d79e4a6cd951bb9e874f244db1f9129a862d
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-11-07 14:05:58 -0500

    Added skeleton structure for setup page

--- app/src/main/java/com/whohatescardio/stubbsnation/whohatescardio/SetupActivity.java
+++ app/src/main/java/com/whohatescardio/stubbsnation/whohatescardio/SetupActivity.java
@@ -1,13 +1,17 @@
 package com.whohatescardio.stubbsnation.whohatescardio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class SetupActivity extends AppCompatActivity {
+    private final int NEW_USER = 1;
+    private final int EDIT_USER = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +20,29 @@ public class SetupActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Bundle extras = getIntent().getExtras();
+        if(extras.getInt("req_code") == EDIT_USER){
+            //TODO: Read from database, and populate fields with exsisting values
+        }
+
+        Button submitButton = (Button) findViewById(R.id.button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //TODO: Grab all the values of the form
+
+
+                //TODO: Create a user object
+
+
+                //TODO: Create an async task to save the new user to a database
             }
         });
+
+
+
+
+
     }
 
 }

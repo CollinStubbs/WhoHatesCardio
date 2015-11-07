3a7cc3485ef74e2de70f79a31afb2066b31a26f0
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-11-07 14:07:27 -0500

    Added User class

---
+++ app/src/main/java/com/whohatescardio/stubbsnation/whohatescardio/User.java
@@ -0,0 +1,67 @@
+package com.whohatescardio.stubbsnation.whohatescardio;
+
+/**
+ * Created by peter on 07/11/15.
+ */
+import java.util.HashMap;
+
+public class User {
+    private String name;
+    private char   sex;
+    private float  age;
+    private float  weight;
+    private float  activityLevelFactor;
+
+    private HashMap<String,Float> alf;
+
+    public User(){
+        this.alf = new HashMap<>();
+        alf.put("sedentary",(float)1.2);
+        alf.put("lightlyActive",(float)1.375);
+        alf.put("moderatelyActive",(float)1.55);
+        alf.put("veryActive",(float)1.725);
+        alf.put("extremelyActive",(float)1.9);
+    }
+
+    private String getName(){
+        return this.name;
+    }
+    private char getSex(){
+        return this.sex;
+    }
+    private float getAge(){
+        return this.age;
+    }
+    private float getWeight(){
+        return this.weight;
+    }
+    private float getActivity(){
+        return this.activityLevelFactor;
+    }
+
+    private void setName(String n){
+        this.name = n;
+    }
+    private void setSex(char s){
+        this.sex = s;
+    }
+    private void setAge(int a){
+        this.age = a;
+    }
+    private void setWeight(float w){
+        this.weight = w;
+    }
+    private void setActivityLevelFactor(String act){
+        float alf = this.alf.get(act);
+        setActivityLevelFactor(alf);
+
+    }
+    private void setActivityLevelFactor(float n){
+        this.activityLevelFactor = n;
+    }
+
+
+
+
+
+}
